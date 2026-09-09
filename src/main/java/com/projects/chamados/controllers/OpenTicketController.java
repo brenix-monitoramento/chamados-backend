2993fd8b825ec2d4235ac86d02b73e27e69e8e1c
Breno Gonçalves <breno.goncalves@datatraffic.com.br>
2026-09-09 14:04:05 -0300

    docs: adiciona documentação dos endpoints de chamados.

--- src/main/java/com/projects/chamados/controllers/OpenTicketController.java
+++ src/main/java/com/projects/chamados/controllers/OpenTicketController.java
@@ -3,6 +3,11 @@ package com.projects.chamados.controllers;
 import com.projects.chamados.dtos.inputs.OpenTicketInputDTO;
 import com.projects.chamados.dtos.outputs.OpenTicketOutputDTO;
 import com.projects.chamados.services.OpenTicketService;
+import com.projects.chamados.utils.Constants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,10 +19,14 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/open-tickets")
+@Tag(name = "Chamados")
+@SecurityRequirement(name = Constants.SECURITY_SCHEME_NAME)
 public class OpenTicketController {
     @Autowired
     private OpenTicketService openTicketService;
 
+    @Operation(description = "Lista todos os chamados ou chamado com base na pesquisa.")
+    @ApiResponse(responseCode = "200")
     @GetMapping
     public ResponseEntity<List<OpenTicketOutputDTO>> getAllBySearch(
             @RequestParam(required = true) String searchTerm
@@ -27,6 +36,8 @@ public class OpenTicketController {
         return ResponseEntity.ok().body(openTickets);
     }
 
+    @Operation(description = "Cadastra novo chamado.")
+    @ApiResponse(responseCode = "201")
     @PostMapping
     public ResponseEntity<OpenTicketOutputDTO> post(@RequestBody @Valid OpenTicketInputDTO openTicket){
         var createdOpenTicket = this.openTicketService.create(openTicket);
@@ -34,6 +45,8 @@ public class OpenTicketController {
         return ResponseEntity.status(HttpStatus.CREATED).body(createdOpenTicket);
     }
 
+    @Operation(description = "Atualiza chamado.")
+    @ApiResponse(responseCode = "200")
     @PutMapping(value = "/{openTicketId}")
     public ResponseEntity<OpenTicketOutputDTO> put(@PathVariable UUID openTicketId, @RequestBody @Valid OpenTicketInputDTO openTicket){
         var updatedOpenTicket = this.openTicketService.updateById(openTicketId, openTicket);
@@ -41,6 +54,8 @@ public class OpenTicketController {
         return ResponseEntity.ok().body(updatedOpenTicket);
     }
 
+    @Operation(description = "Exclui chamado.")
+    @ApiResponse(responseCode = "204")
     @DeleteMapping(value = "/{openTicketId}")
     public ResponseEntity<Void> delete(@PathVariable UUID openTicketId){
         this.openTicketService.deleteById(openTicketId);
